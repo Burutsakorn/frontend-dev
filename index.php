d69942a508f83f444be5603c7866ac428e9a593d
Burutsakorn <71056653+Burutsakorn@users.noreply.github.com>
2021-01-11 16:13:25 +0700

    addicon

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=Hem, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script scr= "js/bootstrap.min.js"></script>
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <link rel="stylesheet" href="icon/css/all.css">
+
+    <link rel="stylesheet" href="/css/mystyle.css">
+    <script src="js/default.js"></script>
+    
+</head>
+<body>
+    <div class ="container">
+        
+    <button  class= "btn btn-primary"><i class="fab fa-accessible-icon"></i></button>
+    <button  class= "btn btn-primary"><i class="fas fa-cheese"></i></button>
+</div>
+<script>
+    $(()=>{
+        //alert("Hello");
+    });
+</script>
+</body>
+</html>
